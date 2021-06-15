57e368f3c2435698c674e5dcaf85dda45ac604d2
Raghuraman Venkataramani <venkataramra@vmware.com>
2021-06-15 15:13:38 +0530

    backend changes done

---
+++ backend/src/main/java/com/loginform/vo/employeeResponse.java
@@ -0,0 +1,35 @@
+package com.loginform.vo;
+
+public class employeeResponse {
+	
+	String Key;
+	String empNumber;
+	String empName;
+	String role;
+	public String getKey() {
+		return Key;
+	}
+	public void setKey(String key) {
+		Key = key;
+	}
+	public String getEmpNumber() {
+		return empNumber;
+	}
+	public void setEmpNumber(String empNumber) {
+		this.empNumber = empNumber;
+	}
+	public String getEmpName() {
+		return empName;
+	}
+	public void setEmpName(String empName) {
+		this.empName = empName;
+	}
+	public String getRole() {
+		return role;
+	}
+	public void setRole(String role) {
+		this.role = role;
+	}
+	
+
+}
---
+++ backend/src/main/java/com/loginform/vo/user.java
@@ -0,0 +1,21 @@
+package com.loginform.vo;
+
+public class user {
+
+	String name;
+	String password;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	
+}
